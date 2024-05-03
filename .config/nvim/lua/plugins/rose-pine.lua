82713f61607aaf8ebdf13bb50b5fee98d172210e
Anders Östlin <anders.ostlin@gmail.com>
2024-05-03 08:14:59 +0200

    deafult theme dawnfox

--- .config/nvim/lua/plugins/rose-pine.lua
+++ .config/nvim/lua/plugins/rose-pine.lua
@@ -1,12 +1,17 @@
+-- local function theme()
+--   return vim.o.background == "light" and "dawnfox" or "duskfox"
+-- end
+
 return {
+  { "shaunsingh/nord.nvim" },
+  { "EdenEast/nightfox.nvim" },
   { "rose-pine/neovim", name = "rose-pine", opts = {
     dark_variant = "moon",
   } },
-  -- Configure LazyVim to load rose-pine
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "dawnfox",
     },
   },
 }
